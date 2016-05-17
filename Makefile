b34c7646d861bfa970280ab49e1025ca42a20aea
leonid <vasiliev.leonid@gmail.com>
2016-05-17 13:58:56 +0000

    13. Runtime linking

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: exe lib
 exe:	hello.h main.c lib
-	gcc main.c -fPIC -L. -lHello -o hello
+	gcc main.c -fPIC -ldl -o hello
 
 lib:	hello.h hello.c
 	gcc -shared hello.c -fPIC -o libHello.so
--- main.c
+++ main.c
@@ -1,8 +1,28 @@
-#include "hello.h" 
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <dlfcn.h>
+
+
+void (*hello_message)(const char *);
+
+bool init_library(){
+	void *hdl = dlopen("./libHello.so",RTLD_LAZY);
+	if (NULL==hdl)
+		return false;
+	hello_message=(void (*)(const char*))dlsym(hdl,"hello_message");
+	if (NULL==hello_message)
+		return false;
+	return true;
+}
+ 
 
 
 int main()
 {
-	hello_message("Vasya");
+	if (init_library())
+		hello_message("Vasya");
+	else
+		printf("error");
 	return 0;
 }
