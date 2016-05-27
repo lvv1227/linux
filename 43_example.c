1aee302e40b1f0af9ab7d00daa2183d28f7961f2
leonid <vasiliev.leonid@gmail.com>
2016-05-27 08:05:52 +0000

    43_example working

--- 43_example.c
+++ 43_example.c
@@ -7,7 +7,7 @@ char child_stack[STACK_SIZE+1]; //allocating array
 
 int child(void *params){
   int c=0;
-  while(true){
+  while(1){
     usleep(50000);
     printf("child turn %d\n",c++);
   }
@@ -18,7 +18,7 @@ int main(){
   int result=clone(child,child_stack+STACK_SIZE,0,0); //the end of the array is used as the stack is movign backward
   printf("clone result=\%d\n",result);
   
-  while(true){
+  while(1){
     usleep(50000);
     printf("parent turn %d\n",c++);
   }
